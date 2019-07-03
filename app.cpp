cb8464becbd8ac950e5ad2a2686d1addde49bba1
nebula <anil100391@gmail.com>
2019-07-03 23:48:58 +0530

    Error handling: The ChernoProject OpenGL Series Episode 10

--- app.cpp
+++ app.cpp
@@ -5,6 +5,35 @@
 #include <fstream>
 #include <sstream>
 
+#ifdef WIN
+    #define ASSERT(x) if (!(x)) __debugBreak();
+    #define GLCall(x) GLClearError();\
+        x;\
+        ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+#endif
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+static void GLClearError()
+{
+    while ( glGetError() != GL_NO_ERROR )
+    {
+    }
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while(GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "):" << function << " " << file << ": " << line << "\n";
+        return false;
+    }
+
+    return true;
+}
+
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 struct ShaderProgramSource
