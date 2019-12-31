b7d627b279fcd021ef9cfb7e750e061f2354be17
Anil Kumar Singh <anil100391@gmail.com>
2019-12-31 15:36:58 +0530

    add transformation matrix to mesh

--- utils/mesh.h
+++ utils/mesh.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include <string>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 struct mesh
@@ -15,6 +18,26 @@ struct mesh
     std::vector<float>         _normals;
     std::vector<unsigned int>  _trias;
     std::vector<unsigned int>  _quads;
+
+    const glm::mat4& GetTransform()
+    {
+        return _modelMat;
+    }
+
+    void SetTransform(const glm::mat4& modelmat)
+    {
+        _modelMat = modelmat;
+    }
+
+    void translate(const glm::vec3& dr)
+    {
+        _modelMat[3][0] + dr[0];
+        _modelMat[3][1] + dr[1];
+        _modelMat[3][2] + dr[2];
+    }
+
+private:
+    glm::mat4                  _modelMat;
 };
 
 #endif // _mesh_h_
