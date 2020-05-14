0942ed619fb8cf8495b47771d2581bedc6ed2e92
orion <anil100391@gmail.com>
2020-05-14 22:28:58 +0530

    cmake update from jason turner's cpp starter project

--- vertexarray.cpp
+++ vertexarray.cpp
@@ -37,7 +37,7 @@ void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout &la
     Bind();
     vb.Bind();
     const auto& elements = layout.GetElements();
-    unsigned int offset = 0;
+    unsigned long offset = 0;
     for ( unsigned int ii = 0; ii < elements.size(); ++ii )
     {
         const auto& element = elements[ii];
